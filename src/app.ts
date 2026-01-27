130fd6de9b0db2a519d2f7432bfdee6def07c556
Ariful <arifulmit17@gmail.com>
2026-01-27 14:00:59 +0600

    added better auth

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
 
 const app=express();
 
 app.use(cors());
 app.use(express.json());
 
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 app.use("/api",routes)
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the SkillBridge Server!");
+});
 
 export default app;
